f36c47aa2959bdcc511b77043c0ed4997fab97a5
YOUR_NAME <YOUR_EMAIL>
2022-03-20 18:57:02 +0600

    create view page

--- crm/views.py
+++ crm/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Orders
 
-# Create your views here.
+def first_page(request):
+    object_list = Orders.objects.all()
+    return render(request, './index.html', {'object_list': object_list})
--- landingsite/urls.py
+++ landingsite/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import first_page
+from crm import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', first_page),
+    path('', views.first_page)
 ]
