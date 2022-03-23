630a1b107e5cb4562131192cd9b8e2a51b06336d
YOUR_NAME <YOUR_EMAIL>
2022-03-23 14:32:39 +0600

    crm, price, telebot

--- crm/models.py
+++ crm/models.py
@@ -1,9 +1,20 @@
 from django.db import models
 
+class StatusCrm(models.Model):
+    Status_name = models.CharField(max_length=200, verbose_name='Название статуса')
+
+    def __str__(self):
+        return self.Status_name
+
+    class Meta:
+        verbose_name = 'Статус'
+        verbose_name_plural = 'Статусы'
+
 class Orders(models.Model):
     order_dt = models.DateTimeField(auto_now=True)
     order_name = models.CharField(max_length=200, verbose_name='Имя')
     order_phone = models.CharField(max_length=200, verbose_name='Телефон')
+    order_status = models.ForeignKey(StatusCrm, on_delete=models.PROTECT, null=True, blank=True, verbose_name='Статус')
 
     def __str__(self):
         return self.order_name
@@ -11,3 +22,16 @@ class Orders(models.Model):
     class Meta:
         verbose_name = 'Заказ'
         verbose_name_plural = 'Заказы'
+
+class CommentCrm(models.Model):
+    comment_binding = models.ForeignKey(Orders, on_delete=models.CASCADE, verbose_name='Заявка')
+    comment_text = models.TextField(max_length=200, verbose_name='Текст комментария')
+    comment_date = models.DateTimeField(auto_now=True, verbose_name='Дата создания')
+
+    def __str__(self):
+        return self.comment_text
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+
--- crm/views.py
+++ crm/views.py
@@ -1,6 +1,37 @@
 from django.shortcuts import render
+
+from .forms import OrderForm
 from .models import Orders
+from cms.models import CmsSlider
+from price.models import PriceCard, PriceTable
+from telebot.sendmessage import sendTelegram
 
 def first_page(request):
-    object_list = Orders.objects.all()
-    return render(request, './index.html', {'object_list': object_list})
+    slider_lists = CmsSlider.objects.all()
+    pc_1 = PriceCard.objects.get(pk=1)
+    pc_2 = PriceCard.objects.get(pk=2)
+    pc_3 = PriceCard.objects.get(pk=3)
+    price_table = PriceTable.objects.all()
+
+    form = OrderForm()
+
+    dic_objects = {'slider_list': slider_lists,
+                   'pc_1': pc_1,
+                   'pc_2': pc_2,
+                   'pc_3': pc_3,
+                   'price_table': price_table,
+                   'form': form
+                   }
+
+    return render(request, './index.html', dic_objects)
+
+def thanks_page(request):
+    if request.POST:
+        name = request.POST['name']
+        phone = request.POST['phone']
+        element = Orders(order_name = name, order_phone = phone)
+        element.save()
+        sendTelegram(tg_name=name, tg_phone=phone)
+        return render(request, './thanks.html', { 'name': name,})
+    else:
+        return render(request, './thanks.html')
